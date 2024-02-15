3a23b9dc6cc35bc142f2ecd4cb9829e28325b83c
Edmond-Wu-9 <115105345+Edmond-Wu-9@users.noreply.github.com>
2024-02-15 10:19:02 -0500

    Added .gitignore and Viewing Excel files

--- VGOR_Fitment_Tool/filter.py
+++ VGOR_Fitment_Tool/filter.py
@@ -3,15 +3,19 @@ This program is meant to take help with the input of Products into the ACES file
 Requires: User Input (Loading List) , Jobber Sheet , and ACES Sheet
 Author = Edmond Wu 
 ''' 
-
-# Take in Loading List through User Input GUI 
-# Creates a file called loading_list 
-# Press Enter to add to loading_list
-# Press Review to check loading_list
-# Press Done when input is done
-
 import tkinter as tk
 from tkinter import messagebox
+import openpyxl 
+from openpyxl import load_workbook
+
+
+''' GUI For Tool
+Take in Loading List through User Input GUI 
+Creates a file called loading_list 
+Press Enter to add to loading_list
+Press Review to check loading_list
+Press Done when input is done
+'''
 
 def save_to_file(event=None): # Save input to file 
     user_input = input_field.get()
@@ -45,4 +49,14 @@ done_button.pack(side=tk.LEFT , fill=tk.X , expand=True)
 review_button = tk.Button(root, text="Review", command=review_file)
 review_button.pack(side=tk.LEFT , fill=tk.X , expand=True)
 
-root.mainloop()
+#root.mainloop()
+
+# Open excel files for reading 
+# Maybe add a function to give the option to upload a file 
+jobber_book = load_workbook('jobber.xlsx')
+#aces_book = load_workbook('aces.xlsm') This shit takes forever to load 
+
+jobber_sheet = jobber_book.active
+
+print('Total number of rows: '+str(jobber_sheet.max_row)+'. And total number of columns: '+str(jobber_sheet.max_column))
+
