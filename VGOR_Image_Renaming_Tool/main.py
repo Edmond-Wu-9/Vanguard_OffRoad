50b888a188a5a0785589f08aa302499ec44eff96
Edmond-Wu-9 <ed.dev.1226@gmail.com>
2024-03-29 10:29:00 -0400

    stuff

--- VGOR_Image_Renaming_Tool/main.py
+++ VGOR_Image_Renaming_Tool/main.py
@@ -1,5 +1,6 @@
 import os 
 import re
+import pandas as pd
 
 #Move directory to where all the Images are 
 image_dir = "C:\\Users\\edmon\\OneDrive\\Desktop\\Images_Filezilla"
@@ -7,24 +8,22 @@ os.chdir(image_dir)
 
 #Gets all the file names into a variable called file_list
 file_list = os.listdir()
-count = 0
 
 #Rename the files from PartNumber.Suffix.jpg/png -> PartNumber_Suffix.jpg/png
-for filename in file_list:
-    if filename.endswith((".jpg" , ".png")):
-        parts = filename.split(".")
-        if len(parts) == 3 and len(parts[1]) == 1:
-            part_number , suffix = parts[0] , parts[1]
-            extension = parts[-1]
-            new_filename = f"{part_number}_{suffix}.{extension}"
-            old_path = os.path.join(image_dir, filename)
-            new_path = os.path.join(image_dir, new_filename)
-            if not os.path.exists(new_path):
-                #os.rename(old_path, new_path)
-                #print(f"Renamed {filename} to {new_filename}")
-                continue
-            else:
-                count = count + 1
-                print(f"Skipped renaming {filename} (file {new_filename} already exists)")
-#rename_period()
-print(f"There are {count} files that have issues if changing the name")
+def rename_period():
+    for filename in file_list:
+        if filename.endswith((".jpg" , ".png")):
+            parts = filename.split(".")
+            if len(parts) == 3 and len(parts[1]) == 1:
+                part_number , suffix = parts[0] , parts[1]
+                extension = parts[-1]
+                new_filename = f"{part_number}_{suffix}.{extension}"
+                old_path = os.path.join(image_dir, filename)
+                new_path = os.path.join(image_dir, new_filename)
+                if not os.path.exists(new_path):
+                    #os.rename(old_path, new_path)
+                    print(f"Renamed {filename} to {new_filename}")
+                    continue
+                else:
+                    print(f"Skipped renaming {filename} (file {new_filename} already exists)")
+rename_period()
