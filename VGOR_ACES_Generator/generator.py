f55f91e18b7aa2a64e2729fcc5044fdb26175f30
Edmond-Wu-9 <115105345+Edmond-Wu-9@users.noreply.github.com>
2024-06-28 23:50:57 -0400

    Creation of GUI

--- VGOR_ACES_Generator/generator.py
+++ VGOR_ACES_Generator/generator.py
@@ -1,3 +1,9 @@
 import pandas as pd 
-import tkinter as tk 
+import tkinter 
 
+# Create a Frame for the GUI
+gui = tkinter.Tk()
+gui.title('ACES Generator')
+gui.geometry('1400x700')
+
+gui.mainloop()
