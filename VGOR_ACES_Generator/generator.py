1e08a65642b6ede76ae3c017723663af37813af5
Edmond-Wu-9 <115105345+Edmond-Wu-9@users.noreply.github.com>
2024-06-29 14:43:27 -0400

    Padding On Output

--- VGOR_ACES_Generator/generator.py
+++ VGOR_ACES_Generator/generator.py
@@ -39,7 +39,7 @@ tk.Label(gui, text="ACES Data File:", font=("Agency FB", 24)).grid(row=2, column
 tk.Entry(gui, textvariable=aces_data_path, width=40).grid(row=2, column=1, padx=10, pady=5)
 tk.Button(gui, text="Browse", command=load_aces_data_file).grid(row=2, column=2, padx=10, pady=5)
 ## Output File Section
-tk.Label(gui, text="Output File Path:", font=("Agency FB", 24)).grid(row=3, column=0, pady=2)
+tk.Label(gui, text="Output File Path:", font=("Agency FB", 24)).grid(row=3, column=0, padx=10, pady=2)
 tk.Entry(gui, textvariable=output_path, width=40).grid(row=3, column=1, padx=10, pady=5)
 tk.Button(gui, text="Save As", command=gen_output_path).grid(row=3, column=2, padx=10, pady=5)
 
